4e846a0ee8788cf5a761dec7de35b244855bfeec
haider8645 <haider_lodhi@hotmail.com>
2018-04-15 19:57:24 +0200

    updated the script to read images and rename them for kipro

--- python-scripts/kipro/read-rename-images.py
+++ python-scripts/kipro/read-rename-images.py
@@ -1,10 +1,6 @@
 from PIL import Image
-import os
 import glob
 from random import shuffle
-import numpy as np
-
-
 
 def shuffle_images(addrs):
     shuffle(addrs)
@@ -15,18 +11,12 @@ def shuffle_images(addrs):
 def save_images(save_location,addresses):
     for i in range (len(addresses)):
         img = Image.open(addresses[i])
-        img.save(save_location +'cardboard2' +str(i), 'PNG')
-
+        img.save(save_location +'shredded' +str(i) + '.png', 'PNG')
     return
 
-
-#Size of images
-IMAGE_WIDTH = 384
-IMAGE_HEIGHT = 384
-
-trash_pics = '/home/haider/Desktop/KIPro Data/Data/data_original/data/data_original/training/b12/*.png'
+trash_pics = '/home/haider/Desktop/KIPro Data/Data/data_transformed/validation/spaene/*.png'
 addrs = glob.glob(trash_pics)
-print 'Length of addrs' 
+print 'Length of addrs'
 print len(addrs)
 
 shuffled_addresses = shuffle_images(addrs)
