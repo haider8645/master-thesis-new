1aaf1cb5b281461f1ce1f96387cbe7d7b652c876
Muhammad Haider Khan Lodhi <lod@pc64.ips.biba.uni-bremen.de>
2018-06-30 20:25:32 +0200

    updated kipro,trashnet lmdb create read scripts

--- python-scripts/LMDB-scripts/create_kipro_trashnet_lmdb_with_labels.py
+++ python-scripts/LMDB-scripts/create_kipro_trashnet_lmdb_with_labels.py
@@ -14,9 +14,9 @@ import lmdb
 def convert_lmdb_to_numpy():
     db_path = '/home/lod/master-thesis/LMDB-datasets/prepared_dataSets/dataSet_5c_250_5_2_eval/img_eval_db/'
    
-    N = 500
+    N = 15000
 
-    x = np.zeros((N, 227, 227, 3 ), dtype=np.uint8)
+    x = np.zeros((N, 3, 227, 227 ), dtype=np.uint8)
     y = np.zeros(N, dtype=np.int64)
     j = 0
 
@@ -24,16 +24,16 @@ def convert_lmdb_to_numpy():
     lmdb_txn = lmdb_env.begin()      # equivalent to mdb_txn_begin()
     lmdb_cursor = lmdb_txn.cursor()  # equivalent to mdb_cursor_open()
     for key, value in lmdb_cursor:
-         datum = caffe.proto.caffe_pb2.Datum()
+         datum = caffe_pb2.Datum()
          datum.ParseFromString(value)
-         image = np.zeros((datum.channels, datum.height, datum.width))
-         image = caffe.io.datum_to_array(datum)
-         image = np.transpose(image, (1, 2, 0))
-         image = image[:, :, (2, 1, 0)]
-         image = image.astype(np.uint8)
-         x[j] = image
+         flat_x = np.fromstring(datum.data, dtype=np.uint8)
+         # flat_x = np.fromstring(datum.data, dtype=np.float32)
+         x[j] = flat_x.reshape(datum.channels, datum.height, datum.width)
+         #image = caffe.io.datum_to_array(datum)
+         #x[j] = image
          y[j] = datum.label
          print y[j]
+         j=j+1
     return x,y
 
 trash_net_pics = '/home/lod/datasets/trashnet/data/dataset-resized/resized_to_227/*.jpg'
@@ -74,24 +74,23 @@ with in_db.begin(write=True) as in_txn:
 
     print 'Now adding KIPro Images'
 
-    x = np.zeros((500, 227, 227, 3 ), dtype=np.uint8)
-    y = np.zeros(500, dtype=np.int64)
+    x = np.zeros((15000,3,227,227 ), dtype=np.uint8)
+    y = np.zeros(15000, dtype=np.int64)
 
     x,y = convert_lmdb_to_numpy()
     j=0
     for in_idx in range (501, 1000):
         
-        print y[j] 
-
+        print 'label',y[j]
+        print 'j',j 
         im = x[j] # or load whatever ndarray you need
-        im = im[:,:,::-1]
-        im = im.transpose((2,0,1))
         im_dat = caffe.io.array_to_datum(im,y[j])
         in_txn.put('{:0>10d}'.format(in_idx), im_dat.SerializeToString())
         print '{:0>5d}'.format(in_idx)
         j=j+1
       
-
+print 'x', len(x)
+print 'y', len(y)
                                  
 in_db.close()
 
--- python-scripts/LMDB-scripts/read_lmdb.py
+++ python-scripts/LMDB-scripts/read_lmdb.py
@@ -1,39 +1,38 @@
 import lmdb
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.image as mpimg
 import cv2
 import os
 import sys
+
+caffe_root = '/home/lod/master-thesis' 
+sys.path.insert(0, caffe_root + 'python')
 import caffe
 
-# Make sure that caffe is on the python path:
-
-
-
-
-caffe_root = '/home/lod/master-thesis'  # this file is expected to be in {caffe_root}/examples
-sys.path.insert(0, caffe_root + 'python')
-db_path = '/home/lod/master-thesis/LMDB-datasets/kipro/test_lmdb/'
+db_path = '/home/lod/datasets/trashnet/data/test_lmdb'
 dirname = '/home/lod/master-thesis/LMDB-datasets/read_lmdb_outputs/'
 
-for i in range(1):
-    x = '0000000800'
-    lmdb_env = lmdb.open(db_path)  # equivalent to mdb_env_open()
-    lmdb_txn = lmdb_env.begin()  # equivalent to mdb_txn_begin()
-    lmdb_cursor = lmdb_txn.cursor()  # equivalent to mdb_cursor_open()
-#    print lmdb_cursor.first()
-#    print lmdb_cursor.key()
-#    print lmdb_cursor.get('00001')
-    lmdb_cursor.get(x) #  get the data associated with the 'key' 1, change the value to get other images
-    value = lmdb_cursor.value()
-    key = lmdb_cursor.key()
-    print key
-    datum = caffe.proto.caffe_pb2.Datum()
-    datum.ParseFromString(value)
-    image = np.zeros((datum.channels, datum.height, datum.width))
-    image = caffe.io.datum_to_array(datum)
-    image = np.transpose(image, (1, 2, 0))
-    image = image[:, :, (2, 1, 0)]
-    image = image.astype(np.uint8)
-    cv2.imwrite(os.path.join(dirname,'test_kipro_0000001000' + str(i) + '.png'), image)
+N = 1000
+
+x = np.zeros((N, 227, 227, 3), dtype=np.uint8)
+y = np.zeros(N, dtype=np.int64)
+j = 0
+
+lmdb_env = lmdb.open(db_path)  # equivalent to mdb_env_open()
+lmdb_txn = lmdb_env.begin()  # equivalent to mdb_txn_begin()
+lmdb_cursor = lmdb_txn.cursor()  # equivalent to mdb_cursor_open()
+for key, value in lmdb_cursor:
+     datum = caffe.proto.caffe_pb2.Datum()
+     datum.ParseFromString(value)
+     image = np.zeros((datum.channels, datum.height, datum.width))
+     image = caffe.io.datum_to_array(datum)
+     image = np.transpose(image, (1, 2, 0))
+     image = image[:, :, (2, 1, 0)]
+     image = image.astype(np.uint8)
+     matplotlib.image.imsave('/home/lod/master-thesis/LMDB-datasets/read_lmdb_outputs/data'+str(j), image)     
+     x[j] = image
+     print j
+     j += 1 
